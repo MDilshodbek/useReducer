97e4c50743ad7f68b5c4ad1d5d945a1f875ed864
MDilshodbek <mdbek55@gmail.com>
2024-05-25 13:52:39 +0900

    complete useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { Button, Input } from "antd";
 
 const action = (state, { type, payload }) => {
-  // if (reducer.type === "INCREMENT") {
-  //   return {
-  //     ...state,
-  //     count: state.count + 1,
-  //   };
-  // }
-  // if (reducer.type === "DECREMENT") {
-  //   return {
-  //     ...state,
-  //     count: state.count + 1,
-  //   };
-  // }
-  // if (reducer.type === "CHANGE") {
-  //   return {
-  //     ...state,
-  //     value: reducer.payload.value,
-  //   };
-  // }
-  // return state;
-
   switch (type) {
-    case "INCREMENT":
-      return { ...state, count: state.count + 1 };
-    case "DECREMENT":
-      return { ...state, count: state.count - 1 };
+    case "DATA_CHANGE":
+      return {
+        ...state,
+        data: payload.data,
+      };
+    case "DELETE":
+      return {
+        ...state,
+        data: state.data.filter(({ id }) => id !== payload.id),
+      };
     case "CHANGE":
       return { ...state, value: payload.value };
 
@@ -36,15 +22,52 @@ const action = (state, { type, payload }) => {
   }
 };
 
-function App() {
+const App = () => {
   const [state, dispatch] = useReducer(action, {
-    count: 0,
-    value: "",
+    data: [],
   });
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      const data = await response.json();
+
+      dispatch({ type: "DATA_CHANGE", payload: { data } });
+    };
+    getData();
+  }, []);
+
+  const onClick = async (id) => {
+    dispatch({ type: "DELETE", payload: { id } });
+
+    await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      mathod: "DELETE",
+      headers: {
+        "Content-Type": "aplication/json",
+      },
+    });
+  };
 
   return (
     <div className="flex flex-col justify-center items-center gap-[10px]">
-      <div className="flex gap-[10px]">
+      {state.data.map(({ id, title }) => (
+        <div key={id}>
+          {title}
+          <Button type="primary" onClick={() => onClick(id)}>
+            Delete
+          </Button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
+
+{
+  /* <div className="flex gap-[10px]">
         <Button type="primary" onClick={() => dispatch({ type: "INCREMENT" })}>
           Plus
         </Button>
@@ -61,9 +84,35 @@ function App() {
           type="text"
         />
         <h3>Value: {state.value}</h3>
-      </div>
-    </div>
-  );
+      </div> */
 }
 
-export default App;
+// case "INCREMENT":
+//   return { ...state, count: state.count + 1 };
+// case "DECREMENT":
+//   return { ...state, count: state.count - 1 };
+// case "CHANGE":
+//   return { ...state, value: payload.value };
+
+// default:
+//   return state;
+
+// if (reducer.type === "INCREMENT") {
+//   return {
+//     ...state,
+//     count: state.count + 1,
+//   };
+// }
+// if (reducer.type === "DECREMENT") {
+//   return {
+//     ...state,
+//     count: state.count + 1,
+//   };
+// }
+// if (reducer.type === "CHANGE") {
+//   return {
+//     ...state,
+//     value: reducer.payload.value,
+//   };
+// }
+// return state;
